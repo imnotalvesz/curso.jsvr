13d595df929559ceb572a835550250c64fc7d254
imnotalvesz <nataly.fernandes.alves@escola.pr.gov.br>
2025-09-25 12:09:18 -0300

    ok

---
+++ exer13.js/exer13.js
@@ -0,0 +1,13 @@
+// Criando um Array
+
+// Declarando um array com filmes favoritos
+const filmes = ["harry potter", "rei do show", "descendentes", "rapunzel"];
+
+// Acessando e imprimindo o segundo elemento do array
+console.log("O segundo filme da lista é:", filmes[1]);
+
+// Adicionando um novo filme ao final do array
+filmes.push("O Senhor dos Anéis");
+
+// Imprimindo o array completo para confirmar a adição
+console.log("Lista completa de filmes:", filmes);
